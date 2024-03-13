c930418f0a9f1fe84f6e7ea7f8be0a5399924777
pgap22 <gerardo.saz120@gmail.com>
2024-03-13 09:35:22 -0600

    xd

--- app/(protected)/home/cart/page.jsx
+++ app/(protected)/home/cart/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { MdOutlineArrowBack } from "react-icons/md";
 
 export default function CartPage() {
@@ -5,7 +6,9 @@ export default function CartPage() {
         <main className="p-4">
             <div className="flex gap-4 items-center">
                 <div className="w-10 rounded-md border aspect-square bg-foreground border-border flex items-center justify-center">
-                    <MdOutlineArrowBack size={24} />
+                    <Link href={"/home"}>
+                        <MdOutlineArrowBack size={24} />
+                    </Link>
                 </div>
                 <h1 className="font-bold text-2xl">Mi Carrito</h1>
             </div>
