13750e48b19dc349cd7928fb91348fe44cc4477e
Gerardo Saz <gerardo.saz120@gmail.com>
2024-03-16 19:46:32 -0600

    xd

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,9 +7,7 @@ const lato = Lato({ subsets: ["latin"], weight: ["400", "700", "900"] });
 export const metadata = {
   title: "SplitQ",
   description: "Recarga, Compra y Disfruta en SplitQ, Descubre una nueva manera de comprar con SplitQ - Una novedosa logistica que facilita tus compras",
-  keywords: {
-    default: ["Splitq", "splitq", "colegio don bosco", "tickets", "don bosco", "colegio don bosco splitq"]
-  }
+  keywords: ["split-q","Splitq", "splitq", "colegio don bosco", "tickets", "don bosco", "colegio don bosco splitq"]
 };
 
 export default function RootLayout({ children }) {
