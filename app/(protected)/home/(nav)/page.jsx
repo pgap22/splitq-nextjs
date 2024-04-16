5f36c64f094482b3d13ed03f5ce0f2aad78ad0c7
JoackoF <fuentesmo.jaf@gmail.com>
2024-04-16 13:53:48 -0600

    Momingo momingueros 👻

--- app/(protected)/home/(nav)/page.jsx
+++ app/(protected)/home/(nav)/page.jsx
@@ -4,15 +4,16 @@ import { Logo } from "@/components/Logo";
 import SettingButtonUser from "@/components/buttons/SettingButtonUser";
 import ThemeToggle from "@/components/buttons/theme-toggle";
 import { authUser } from "@/lib/authUser";
-import { MdFastfood, MdIcecream, MdLandscape, MdOutlineFastfood, MdOutlineLandscape, MdOutlineLocalCafe, MdOutlineLocalOffer, MdOutlineLunchDining, MdOutlineSportsBar } from "react-icons/md";
+import { MdFastfood, MdIcecream, MdLandscape, MdOutlineFastfood, MdOutlineLandscape, MdOutlineLocalCafe, MdOutlineLocalOffer, MdOutlineLunchDining, MdOutlineSportsBar, MdOutlineStore, MdOutlineVerifiedUser } from "react-icons/md";
 import { getProducts } from "@/actions/getProducts";
 import { getCategories } from "@/actions/categories";
-import utils from "util"
+import { getSellers } from "@/actions/getSellers";
 
 export default async function Home() {
     const user = await authUser();
     const product = await getProducts();
     const categorie = await getCategories();
+    const sellers = await getSellers();
     const balance = await getBalance();
     //Por veces no agarraba el logout asiq esto lo solucionaxd
     async function logout() {
@@ -34,7 +35,8 @@ export default async function Home() {
                     <p className="text-text-secundary text-md">Tu saldo actual para comprar productos dentro de <span className="font-bold text-gradient-principal text-gradient bg-gradient-principal">SplitQ</span></p>
                 </div>
             </section>
-            <div className="flex mt-8 gap-4 overflow-auto">
+            <h1 className="text-2xl mt-4 font-bold">Categorias</h1>
+            <div className="flex mt-4 gap-4 overflow-auto">
                 {
                     categorie.map(item => <CategorieCard categorie={item} />)
                 }
@@ -43,25 +45,28 @@ export default async function Home() {
                 <h1 className="text-2xl font-bold">Favoritos de la comunidad</h1>
                 <div className="flex-col gap-4">
                     {
-                        product.map(item => <ProductCard product={item} />)
+                        sellers.map(item => <SellerCard seller={item} />)
                     }
                 </div>
             </div>
         </>
     )
 }
-const ProductCard = ({ product }) => {
+const SellerCard = ({ seller }) => {
     return (
         <div className="min-w-48 mt-4">
             <div className="flex items-center border-border border rounded bg-foreground ">
 
-                {product.images.length ? <img className="w-20 h-20 object-cover rounded border border-border" src={product.images[0].url} />
+                {/* {product.images.length ? <img className="w-20 h-20 object-cover rounded border border-border" src={product.images[0].url} />
                     : <div className="w-20 h-20 flex items-center justify-center">
                         <MdOutlineLocalOffer size={30} />
-                    </div>}
-                <div className="mt-4 p-2">
-                    <h2 className="text-center ">{product.description}</h2>
-                    <p className="text-center font-bold">{product.name}</p>
+                    </div>} */}
+                <div className="w-20 h-20 flex items-center justify-center">
+                    <MdOutlineStore size={30} />
+                </div>
+                <div className="p-2">
+                    <p className="font-bold">{seller.name}</p>
+                    {/* <h2 className="text-center ">{seller.email}</h2> */}
                 </div>
             </div>
         </div>
