21d1603d58df218d9c5046cfe1291c7e8d2d535e
Gerardo Saz <gerardo.saz120@gmail.com>
2024-05-09 18:23:46 -0600

    fix el punto

--- auth.config.js
+++ auth.config.js
@@ -1,31 +1,31 @@
-import Credentials from "next-auth/providers/credentials"
-import bcryptjs from "bcryptjs"
+import Credentials from "next-auth/providers/credentials";
+import bcryptjs from "bcryptjs";
 import { getUserByEmail } from "./lib/user";
 import { AuthError } from "next-auth";
 
 export default {
-    trustHost: true,
-    providers: [Credentials({
+  trustHost: true,
+  providers: [
+    Credentials({
+      async authorize(data) {
+        const { email, password, verificationLogin } = data;
 
-        async authorize(data) {
-            const { email, password, verificationLogin } = data;
+        const user = await getUserByEmail(email);
+        console.log("----", user, "-------");
 
-            const user = await getUserByEmail(email);
+        if (!user) return null;
 
-            if(user.error) throw new Error("xd")
-             
-            if (!user) return null
-            
-            if(verificationLogin) return user;
+        if (user.error) throw new Error("xd");
 
+        if (verificationLogin) return user;
 
-            if(user.token) throw new AuthError("Bad Verification")
+        if (user.token) throw new AuthError("Bad Verification");
 
-            const isSamePassword = await bcryptjs.compare(password, user.password)
+        const isSamePassword = await bcryptjs.compare(password, user.password);
 
-            if (!isSamePassword) return null
-            return user
-        }
-
-    })]
-}
+        if (!isSamePassword) return null;
+        return user;
+      },
+    }),
+  ],
+};
