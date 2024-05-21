faac43ea5851784cd2bd44f5747b6f64d54dc196
Gerardo Saz2 <gerardo.saz120@gmail.com>
2024-05-21 14:22:32 -0600

    logs improve

--- app/(protected)/home/history/page.jsx
+++ app/(protected)/home/history/page.jsx
@@ -7,6 +7,7 @@ import ActionItem from "./ActionItem";
 export default async function HistoryPage() {
   const { id } = await authUser();
   const history = await getHistoryByUserId(id);
+
   return (
     <>
       <main>
@@ -15,11 +16,17 @@ export default async function HistoryPage() {
           <h1 className="font-bold my-4 text-2xl">Historial de Acciones</h1>
           {history.map((day) => (
             <div className="border border-border mb-4 rounded-md bg-foreground">
-              <p className="font-bold px-4 mt-4 pb-0 text-text-secundary capitalize text-lg">
-                {showEsDate(day.date, false)}
-              </p>
+              <div className="flex gap-4 mt-4 items-center">
+                <p className="font-bold px-4 pb-0 text-text-secundary capitalize text-lg">
+                  {showEsDate(day.date, false)}
+                </p>
+                <div className="flex gap-2">
+                  <p className="font-bold ">Dinero Gastado: <span className="text-red-500">${day.actions.filter(action => action.type == "purchase").reduce((total, action)=>(total+action.value.price),0)}</span></p>
+                  <p className="font-bold">Saldo recibido: <span className="text-green-500">${day.actions.filter(action => action.type == "recharge").reduce((total, action)=>(total+action.value),0)}</span></p>
+                </div>
+              </div>
               {day.actions.map((action) => (
-               <ActionItem action={action} />
+                <ActionItem action={action} />
               ))}
             </div>
           ))}
